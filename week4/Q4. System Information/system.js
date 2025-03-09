11eda2bb16ae6673a5ad9b9aa9c149026bd46b08
Gaius Okoase <gaiusonosetale@gmail.com>
2025-03-09 15:13:43 +0100

    Add system.js to retrieve and display system information; update package.json with new start script

---
+++ week4/Q4. System Information/system.js
@@ -0,0 +1,17 @@
+const os = require('os');
+
+let systemInformation = {
+    'Operating System': os.type(),
+    'Architecture': os.arch(),
+    'Platform': os.platform(),
+    'Hostname': os.hostname(),
+    'Total Memory': (os.totalmem() / 1024 ** 3).toFixed(2) + ' GB',
+    'Free Memory': (os.freemem() / 1024 ** 3).toFixed(2) + ' GB',
+    'Uptime': os.uptime() + ' seconds',
+    'CPU Model': os.cpus()[0].model,
+    'Release': os.release(),
+    'Version': os.version()
+};
+console.log(systemInformation);
+
+//FINALLY DONE!! 😫😪
