7d4ff9d8f770824f9e08e50a8a7e799cfd925de1
Gaius Okoase Oreoluwa <gaiusonosetale@gmail.com>
2025-03-21 04:51:34 +0100

    Update index.js with necessary comments

--- week5/index.js
+++ week5/index.js
@@ -1,8 +1,9 @@
+//Implements class ClassRoom and assign maxStudentSize to _maxStudentSize
 export default class ClassRoom {
     constructor(maxStudentSize) {
         this._maxStudentSize = maxStudentSize;
     }
 }
-
+//Creates an instance of class ClassRoom with maximu student size of 10
 const classRoom1 = new ClassRoom(10);
 console.log(classRoom1._maxStudentSize);
