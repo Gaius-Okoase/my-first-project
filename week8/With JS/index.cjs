ff08949e13b06e0465fef3cad6826cfd93a703e2
Gaius Okoase <gaiusonosetale@gmail.com>
2025-04-12 07:37:13 +0100

    Add file creation logic and initial content to index.cjs to use CommonJS modules

---
+++ week8/With JS/index.cjs
@@ -0,0 +1,20 @@
+//* Importing the required modules
+const fs = require('fs');
+const path = require('path');
+
+//* Creating a directory if it doesn't exist, creating a file, and writing a file in it in one go.
+fs.mkdir ('Files', {recursive: true}, (err) => {
+    if (err) {
+        console.log('ERROR:', err)
+        return;
+    } else {
+        console.log("Directory created successfully!");
+    }
+
+    //* Creating the file and writing into it.
+    fs.writeFile('./Files.hello.txt', 'Hello, File System.', 'utf-8', (err) => {
+        if (err) throw err;
+        console.log('File created and written into successfully!');
+        return;
+    }); 
+});
