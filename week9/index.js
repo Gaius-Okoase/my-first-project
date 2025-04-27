e01cb9cd029b3922ae9ce93cc228d88e6c699529
Gaius Okoase <gaiusonosetale@gmail.com>
2025-04-27 09:13:30 +0100

    Add module.js with areaOfRectangle and add functions; create index.js to demonstrate usage

---
+++ week9/index.js
@@ -0,0 +1,5 @@
+import {areaOfRectangle, add} from "./module.js";
+
+console.log(areaOfRectangle(5, 7));
+
+console.log(add(3.5, -33));
---
+++ week9/module.js
@@ -0,0 +1,10 @@
+//Using ES6 syntax
+
+export function areaOfRectangle (length, breadth) {
+     let area = length * breadth;
+     return `Area is ${area}cm².`
+}
+
+export function add (a, b) {
+     return a + b;
+ }
