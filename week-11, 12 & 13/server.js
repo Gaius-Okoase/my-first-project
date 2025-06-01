c2dabfdb4d977edb6347f6b661e168a5cb15095f
Gaius Okoase <gaiusonosetale@gmail.com>
2025-06-01 14:07:32 +0100

    Add user and search routes to server.js for Week 13 assignment

--- week-11, 12 & 13/server.js
+++ week-11, 12 & 13/server.js
@@ -9,6 +9,31 @@ app.get('/', (req, res) => {
     res.send(`Hello, Express!`);
 });
 
+//* Week 13 Assignment - (a) Defining a user/:id route
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    // if(!id){
+    //     return res.status(400).json({error: "Please input user id."});
+    // }
+
+    return res.status(200).json({
+        id: id,
+        name: "Goat"
+    });
+});
+
+//* Week 13 Assignment - (b) Defining a /search route
+app.get('/search', (req, res) => {
+     const name = req.query.name;
+    // if(!name){
+    //     return res.status(400).json({error: "Please input user name."});
+    // }
+
+    return res.status(200).json({
+        id: 1,
+        name: name
+    });
+});
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
