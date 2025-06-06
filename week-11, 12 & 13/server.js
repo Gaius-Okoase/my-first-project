4622e0fb01a7e56e40acd24cacce6ac7e93e7a7e
Gaius Okoase <gaiusonosetale@gmail.com>
2025-06-06 14:24:33 +0100

    Add middleware to log request and handle global errors

--- week-11, 12 & 13/server.js
+++ week-11, 12 & 13/server.js
@@ -2,7 +2,15 @@ const express = require('express'); // Import express.js
 
 const app = express(); //Instantiate an express.js app
 
-PORT = 5000; //Set up port to listen to server
+const PORT = 5000; //Set up port to listen to server
+
+// Middleware to parse json
+app.use(express.json());
+//* Week 13 Middlewares - (a) Middleware to log requests
+app.use((req, res, next) => {
+    console.log(`Received request for ${req.url} with ${req.method}.`);
+    next();
+}) 
 
 app.get('/', (req, res) => {
     console.log(`Received request for ${req.url}`);
@@ -12,9 +20,9 @@ app.get('/', (req, res) => {
 //* Week 13 Assignment - (a) Defining a user/:id route
 app.get('/user/:id', (req, res) => {
     const id = req.params.id;
-    // if(!id){
-    //     return res.status(400).json({error: "Please input user id."});
-    // }
+    if(!id){
+        return res.status(400).json({error: "Please input user id."});
+    }
 
     return res.status(200).json({
         id: id,
@@ -25,15 +33,23 @@ app.get('/user/:id', (req, res) => {
 //* Week 13 Assignment - (b) Defining a /search route
 app.get('/search', (req, res) => {
      const name = req.query.name;
-    // if(!name){
-    //     return res.status(400).json({error: "Please input user name."});
-    // }
+    if(!name){
+        return res.status(400).json({error: "Please input user name."});
+    }
 
     return res.status(200).json({
         id: 1,
         name: name
     });
 });
+
+//* Week 13 Middlewares - (b) Middleware to handle errors
+app.use((err, req, res, next) => {
+    console.error('Error:', err);
+    return res.status(500).json({error: "Sorry, something went wrong. Please try again."});
+    next();
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
