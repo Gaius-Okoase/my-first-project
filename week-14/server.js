4600f1b105bde430f372811da7e631af13cdea80
Gaius Okoase <gaiusonosetale@gmail.com>
2025-06-07 04:45:43 +0100

    Add code logic to connect to database in server.js

---
+++ week-14/server.js
@@ -0,0 +1,28 @@
+// Import all necessary packages/modules
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+// Access .env
+dotenv.config();
+// Instantiate express
+const app = express();
+
+// Set up PORT and MONGP_URI
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI)
+.then(() => {
+    console.log(`Successfully connected to MongoDB.`);
+})
+.catch((error) => {
+    console.log('Failed to connect to the database.');
+    console.error('Error: ', error);
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on localhost:' + PORT);
+});
+
