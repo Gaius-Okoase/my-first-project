74baf43dbf9227b45f76edbb4b6f7f70110c3abf
Gaius Okoase <gaiusonosetale@gmail.com>
2025-06-19 03:24:12 +0100

    feat: initialize project with Express server and MongoDB connection
    - Created package.json with essential dependencies and scripts
    - Set up server.js to configure Express, dotenv, and MongoDB connection
    - Added middleware for JSON parsing and request logging
    - Configured server to listen on specified PORT

---
+++ week-15/server.js
@@ -0,0 +1,27 @@
+// Import all necessary modules
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './config/db.js';
+
+// Instantiate express 
+const app = express();
+// Configure dotenv
+dotenv.config();
+// Connect server to database
+connectDb();
+// Set up PORT
+const PORT = process.env.PORT || 5000;
+
+//! Middlewares
+//Middleware to parse JSON data
+app.use(express.json());
+// Middleware to log request to console
+app.use((req, res, next) => {
+  console.log(`Received request for ${req.url} with ${req.method}`);
+  next(); //* If you don't call next, your server won't move to the next step!
+});
+
+// Set up port to listen to server 
+app.listen(PORT, () => {
+  console.log(`Server started on localhost:${PORT}`);
+});
