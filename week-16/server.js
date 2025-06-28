6948c136a2d22f9c7c573df34247d93efd8928f8
Gaius Okoase <gaiusonosetale@gmail.com>
2025-06-28 11:27:13 +0100

    feat: add axios for fetching cat facts and implement caching logic with Redis

--- week-16/server.js
+++ week-16/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import axios from "axios";
 import client from "./config/db.js";
 
 const app = express();
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
     next();
 });
 
+//! The logics below should be in a controller file when dealing with more complex or serious setups
 //Logic to set key-value pair
 app.get('/set', async (req, res) => {
     try {
@@ -31,6 +33,32 @@ app.get('/get', async (req, res) => {
         return res.status(200).json(`Language added: ${language}`);
     } catch (error) {
         console.log('Error getting key-value pair on Redis', error);
+        return res.status(500).json({error: error.message});
+    }
+});
+
+//Logic to fetch data from mock API
+app.get('/cat-facts', async (req, res) => {
+    try {
+        // Check if key data exists on redis
+        const key = "Cat Fact"
+        const value = await client.get(key);
+        if(value){
+            console.log('Cache hit');
+            return res.status(200).json(value);
+        } else {
+            console.log('Cache miss')
+        }
+
+        const fetchFromApi = await axios.get('https://catfact.ninja/fact');
+        const catFact = fetchFromApi.data;
+
+        // Set new key-value pair on redis
+        await client.set(key, JSON.stringify(catFact));
+        return res.status(200).json({message: catFact});
+        
+    } catch (error) {
+        console.log('Error getting cat facts', error);
         return res.status(500).json({error: error.message});
     }
 })
